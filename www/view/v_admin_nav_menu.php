75ccb9f900f70a6fb7342761d9d0c845b9937fd7
Pavlinus <pavlin.kov@mail.ru>
2016-09-05 20:40:55 +0300

    replacement files

--- www/view/v_admin_nav_menu.php
+++
@@ -1,8 +0,0 @@
-﻿<nav class="menu">
-	<ul>
-		<li class="head">Меню</li>
-		<li><a href="index.php?c=admin">Главная</a></li>
-		<li><a href="index.php?c=admin&act=addGame">Добавить игру</a></li>
-		<li><a href="">Выход</a></li>
-	</ul>
-</nav>
